d0797a409b075936d427871e686987ee4b715e9d
Mario Camara <angelmarito1705@gmail.com>
2024-08-22 17:29:08 -0600

    nuevas funciones de iteracion

--- 02-Mario.js
+++ 02-Mario.js
@@ -5,22 +5,69 @@ const A = [
     [5, 6, 7, 8]
   ];
   
-  const datosArreglo = () => {
+  //.map
+ /* const datosArreglo = () => {
     A.map((datos, index) => {
       datos.map((valores, i) => {
         console.log(`${i}=${valores}`);
       });
     });
   };
-  
-  datosArreglo(); 
+*/ 
 
   //for of
 
+  /*
+const datosArreglo = () => {
+  for (const datos of A) {
+    for (const valores of datos) {
+      console.log(`${datos.indexOf(valores)}=${valores}`)
+    }
+  }
+}
+  */
+
   //for in
 
+  /*
+const datosArreglo = () => {
+  for (const index in A) {
+    const datos = A[index]
+    for (const propiedad in datos) {
+      console.log(`${propiedad}=${datos[propiedad]}`)
+    }
+  }
+}
+  */
+
   //for each
+  /*
+
+const datosArreglo = () => {
+  A.forEach((datos, index) => {
+    datos.forEach((valor, propiedad) => {
+      console.log(`${propiedad}=${valor}`)
+    })
+  })
+}
+  */
 
   //funcion flecha
-
+  /*
+const datosArreglo = () => {
+  A.forEach(datos => datos.forEach((valor, propiedad) => console.log(`${propiedad}=${valor}`)))
+}
+  */
   //while
+const datosArreglo = () => {
+  let i = 0
+  while (i<A.length) {
+    let j = 0
+    while (j<A[i].length) {
+      console.log(`${j}=${A[i][j]}`)
+      j++
+    }
+    i++
+  }
+}
+  datosArreglo();
